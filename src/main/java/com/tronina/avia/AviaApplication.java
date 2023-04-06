cc636a5efcb8d2b6441ddd2a876e862ef53bb850
Tronina Liliya <lily.kmtd@gmail.com>
2023-04-06 21:38:02 +0400

    base

---
+++ src/main/java/com/tronina/avia/AviaApplication.java
@@ -0,0 +1,13 @@
+package com.tronina.avia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tronina/avia/AviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tronina.avia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
