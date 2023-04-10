589636ed796c9c3ab37a4df544b2a8cd26e5c720
Tronina Liliya <lily.kmtd@gmail.com>
2023-04-10 23:01:42 +0400

    p.3

--- src/main/java/com/tronina/avia/service/impl/AirlineService.java
+++ src/main/java/com/tronina/avia/service/impl/AirlineService.java
@@ -1,14 +1,32 @@
 package com.tronina.avia.service.impl;
 
 import com.tronina.avia.entity.Airline;
+import com.tronina.avia.entity.Airplane;
 import com.tronina.avia.repository.AirlineRepository;
+import com.tronina.avia.repository.AirplaneRepository;
 import com.tronina.avia.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AirlineService extends AbstractService<Airline, AirlineRepository> {
 
     public AirlineService(AirlineRepository repository) {
         super(repository);
+    }
+
+    @Autowired
+    private AirplaneRepository airplaneRepository;
+
+    public List<Airplane> findPlanesOfAirline(Long id) {
+        return airplaneRepository.findAllByAirlineId(id);
+    }
+
+    public Airplane addPlaneToAirline(Long id, Airplane element) {
+        Airline airline = findById(id);
+        airline.getAirplanes().add(element);
+        return airplaneRepository.save(element);
     }
 }
