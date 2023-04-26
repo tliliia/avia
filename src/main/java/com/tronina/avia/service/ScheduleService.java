aa95d19a07eb504a79bebb8d4d8c44126da1b981
tronina <t.liliya90@mail.ru>
2023-04-26 16:42:40 +0400

    2 p.7,8

---
+++ src/main/java/com/tronina/avia/service/ScheduleService.java
@@ -0,0 +1,19 @@
+package com.tronina.avia.service;
+
+import com.tronina.avia.service.impl.ReservationCleaningService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ReservationCleaningService service;
+
+    //(секунда, минута, час, день месяца, месяц, день недели), * - любое
+    //каждые 5 минут.
+    @Scheduled(cron = "0 */5 * * * *")
+    public void cleanAnonymousCustomers() {
+        service.cleanExpiredReservations();
+    }
+}
